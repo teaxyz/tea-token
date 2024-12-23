f311feabfd61142454d6769b573134ddc086b40f
Noah Marconi <noah.marconi@gmail.com>
2024-12-23 13:02:02 -0500

    mint all on deploy, remove global cap

--- src/TeaToken/Tea.sol
+++ src/TeaToken/Tea.sol
@@ -19,21 +19,14 @@ import { ERC20Votes } from "@openzeppelin/token/ERC20/extensions/ERC20Votes.sol"
 /* solhint-disable no-unused-import */
 import { EIP712 } from "@openzeppelin/utils/cryptography/EIP712.sol";
 import { ERC20 } from "@openzeppelin/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/access/Ownable.sol";
 /* solhint-enable no-unused-import */
-import { OwnableRoles } from "@solady/auth/OwnableRoles.sol";
+import { Ownable2Step } from "@openzeppelin/access/Ownable2Step.sol";
 import { ERC20Burnable } from "@openzeppelin/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract Tea is OwnableRoles, ERC20Votes, ERC20Burnable {
-    /* --------------------------------- Errors --------------------------------- */
-
-    error MaxSupplyReached();
-
+contract Tea is Ownable2Step, ERC20Votes, ERC20Burnable {
     /* -------------------------------- Constants ------------------------------- */
-
-    uint256 public constant MINTER_ROLE = 1 << 0; // 01 bitmap flag
-    uint256 public constant FACTORY_ROLE = 1 << 1; // 10 bitmap flag
-
-    uint256 public constant MAX_SUPPLY = 10_000_000_000 ether;
+    uint256 public constant INITIAL_SUPPLY = 10_000_000_000 ether;
 
     /* --------------------------------- Globals -------------------------------- */
 
@@ -41,8 +34,14 @@ contract Tea is OwnableRoles, ERC20Votes, ERC20Burnable {
 
     /* ------------------------------- Constructor ------------------------------ */
 
-    constructor(address initialGovernor_) ERC20("Tea Token", "TEA") EIP712("Tea Token", "1") {
-        _initializeOwner(initialGovernor_);
+    constructor(address initialGovernor_)
+        ERC20("Tea Token", "TEA")
+        EIP712("Tea Token", "1")
+        Ownable(initialGovernor_)
+    {
+        totalMinted = INITIAL_SUPPLY;
+
+        _mint(initialGovernor_, INITIAL_SUPPLY);
     }
 
     function _update(address from, address to, uint256 value) internal virtual override(ERC20Votes, ERC20) {
@@ -52,48 +51,15 @@ contract Tea is OwnableRoles, ERC20Votes, ERC20Burnable {
     /* ------------------------------- Mint / Burn ------------------------------ */
 
     function mintTo(address account, uint256 value) public {
-        _checkOwnerOrRoles(MINTER_ROLE);
+        _checkOwner();
 
-        // Cache.
-        uint256 newTotalMinted = totalMinted + value;
-        // Enforce cap on total minted.
-        if (newTotalMinted > MAX_SUPPLY) {
-            revert MaxSupplyReached();
-        }
+        totalMinted = totalMinted + value;
 
         _mint(account, value);
-
-        totalMinted = newTotalMinted;
     }
 
     function burnFrom(address account, uint256 value) public override {
         if (account != msg.sender) _spendAllowance(account, msg.sender, value);
         _burn(account, value);
-    }
-
-    /* ------------------------------- Role Admin ------------------------------- */
-
-    function addMinter(address toAdd) external {
-        _checkRoles(FACTORY_ROLE);
-
-        _grantRoles(toAdd, MINTER_ROLE);
-    }
-
-    function removeMinter(address toRemove) external {
-        _checkOwner();
-
-        _removeRoles(toRemove, MINTER_ROLE);
-    }
-
-    function addFactory(address toAdd) external {
-        _checkOwner();
-
-        _grantRoles(toAdd, FACTORY_ROLE);
-    }
-
-    function removeFactory(address toRemove) external {
-        _checkOwner();
-
-        _removeRoles(toRemove, FACTORY_ROLE);
     }
 }
--- test/TeaToken.sol
+++ test/TeaToken.sol
@@ -16,8 +16,7 @@ contract TeaTokenTest is PRBTest, StdCheats {
     VmSafe.Wallet internal alice = vm.createWallet("Alice Account");
     VmSafe.Wallet internal bob = vm.createWallet("Bob Account");
 
-    error Unauthorized();
-    error MaxSupplyReached();
+    error OwnableUnauthorizedAccount(address account);
     error ERC20InsufficientAllowance(address spender, uint256 allowance, uint256 needed);
 
     function setUp() public virtual {
@@ -31,104 +30,21 @@ contract TeaTokenTest is PRBTest, StdCheats {
     }
 
     function test_mint_fail() public {
-        vm.expectRevert(Unauthorized.selector);
+        vm.expectRevert(abi.encodeWithSelector(OwnableUnauthorizedAccount.selector, address(this)));
         tea.mintTo(alice.addr, 1);
-    }
-
-    function test_addMinter_fail() public {
-        vm.expectRevert(Unauthorized.selector);
-        tea.addMinter(alice.addr);
-    }
-
-    function test_addFactory_fail() public {
-        vm.expectRevert(Unauthorized.selector);
-        tea.addFactory(alice.addr);
-    }
-
-    function test_removeMinter_fail() public {
-        vm.prank(initialGovernor.addr);
-        tea.addFactory(alice.addr);
-
-        vm.prank(alice.addr);
-        tea.addMinter(bob.addr);
-
-        vm.expectRevert(Unauthorized.selector);
-        tea.removeMinter(bob.addr);
-    }
-
-    function test_removeFactory_fail() public {
-        vm.prank(initialGovernor.addr);
-        tea.addFactory(alice.addr);
-
-        vm.expectRevert(Unauthorized.selector);
-        tea.removeFactory(alice.addr);
     }
 
     function test_mint_succeed() public {
         vm.prank(initialGovernor.addr);
-        tea.addFactory(address(this));
-
-        tea.addMinter(bob.addr);
-
-        vm.prank(bob.addr);
         tea.mintTo(alice.addr, 1);
 
-        assertEq(tea.totalSupply(), 1);
-        assertEq(tea.totalMinted(), 1);
+        assertEq(tea.totalSupply(), tea.INITIAL_SUPPLY() + 1);
+        assertEq(tea.totalMinted(), tea.INITIAL_SUPPLY() + 1);
         assertEq(tea.balanceOf(alice.addr), 1);
-    }
-
-    function test_mint_to_cap() public {
-        vm.prank(initialGovernor.addr);
-        tea.addFactory(address(this));
-
-        tea.addMinter(bob.addr);
-
-        uint256 maxSupply = tea.MAX_SUPPLY();
-
-        vm.prank(bob.addr);
-        tea.mintTo(alice.addr, maxSupply);
-
-        assertEq(tea.totalSupply(), maxSupply);
-        assertEq(tea.totalMinted(), maxSupply);
-        assertEq(tea.balanceOf(alice.addr), maxSupply);
-    }
-
-    function test_mint_fail_above_cap() public {
-        vm.prank(initialGovernor.addr);
-        tea.addFactory(address(this));
-
-        tea.addMinter(bob.addr);
-
-        uint256 maxSupply = tea.MAX_SUPPLY();
-
-        vm.prank(bob.addr);
-        tea.mintTo(alice.addr, maxSupply);
-
-        assertEq(tea.totalSupply(), maxSupply);
-        assertEq(tea.totalMinted(), maxSupply);
-        assertEq(tea.balanceOf(alice.addr), maxSupply);
-
-        vm.prank(alice.addr);
-        tea.burnFrom(alice.addr, 1);
-
-        vm.startPrank(bob.addr);
-        vm.expectRevert(MaxSupplyReached.selector);
-        tea.mintTo(alice.addr, 1);
-        vm.stopPrank();
-
-        assertEq(tea.totalSupply(), maxSupply - 1);
-        assertEq(tea.totalMinted(), maxSupply);
-        assertEq(tea.balanceOf(alice.addr), maxSupply - 1);
     }
 
     function test_burn_fail() public {
         vm.prank(initialGovernor.addr);
-        tea.addFactory(address(this));
-
-        tea.addMinter(bob.addr);
-
-        vm.prank(bob.addr);
         tea.mintTo(alice.addr, 1);
 
         vm.expectRevert(abi.encodeWithSelector(ERC20InsufficientAllowance.selector, address(this), 0, 1));
